b80c9f76f7ad27c363fd9122ab2d846e49269daa
PratapKumar <kprathap23@gmail.com>
2018-09-21 16:35:37 +0530

    update defualt first page

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -7,6 +7,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class PostgresDemoApplication {
+	
+	
+	@RequestMapping("/")
+	public void home() {
+		
+		return "Home Page";
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PostgresDemoApplication.class, args);
 	}
