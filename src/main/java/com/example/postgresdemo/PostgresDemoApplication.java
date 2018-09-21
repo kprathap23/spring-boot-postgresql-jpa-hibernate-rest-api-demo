a2d2b8395648186d47700e1bf3ac1cef12916b7d
PratapKumar <kprathap23@gmail.com>
2018-09-21 16:42:38 +0530

    imports updated

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -3,6 +3,8 @@ package com.example.postgresdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableJpaAuditing
